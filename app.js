381c37b30eeae49c31dab3577b2abec489b6295b
LUIS FELDMAN <luisf@MacBook-Pro-Luis.local>
2023-01-17 13:17:30 -0300

    mercado lievre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.listen(PORT, () =>
+    console.log(`server listening on ${PORT}`));
+
+
